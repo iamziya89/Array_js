1afe9bc48d7a504ccf144f1cf35a261fdee0c826
Mohd Ziya <iamziya53@gmail.com>
2022-07-06 18:29:15 -0400

    Practice on array

--- Tuesday_session/array_03.js
+++ Tuesday_session/array_03.js
@@ -3,7 +3,17 @@ let num=['Mohd','Ziya','Rajesh','Prakash'];
 // console.log(num.push('Akmal'));
 // console.log(num)
 
-console.log(num.pop());
+// console.log(num.pop());
+// console.log(num);
+// console.log(num.pop(2));
+// console.log(num)
+
+// num.pop();
+num.pop();
+num.pop();
+num.pop();
+num.pop();
 console.log(num);
-console.log(num.pop(2));
-console.log(num)
+num.push(1);
+num.push(2);
+console.log(num);
