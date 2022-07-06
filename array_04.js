a17d945b80b1b8b2d7e477c2ad46893f934a5a76
Mohd Ziya <iamziya53@gmail.com>
2022-07-05 20:18:41 -0400

    practice on array

---
+++ array_04.js
@@ -0,0 +1,15 @@
+let names = ['vasanth', 'madan','ziya','madan',10,true];
+
+let rajniFamily ={
+    name : 'ranji',
+    age : 75,
+    daughters:{
+        name:'Aishwarya',
+        age:40
+    }
+}
+// console.log(names[0]);
+// console.log(names[1]);
+// console.log(names[2]);
+console.log(names[5]);
+console.log(names);
