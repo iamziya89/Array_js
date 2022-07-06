485bf16f9b63ba089d0ab078f014a6c0d742d2e1
Mohd Ziya <iamziya53@gmail.com>
2022-07-06 16:04:43 -0400

    Practice on array

---
+++ Tuesday_session/array_02.js
@@ -0,0 +1,9 @@
+// let nums=[1,3,4,5,7,89,74,72];
+// console.log(nums[0],nums[6]);
+// let nums=new Array(1,3,4,5,7,89,74,72,100);
+// console.log(nums[0],nums[nums.length-1]);
+
+
+//...........How to add element in array...........
+
+let nums=[1,2,3,4,5,6];
