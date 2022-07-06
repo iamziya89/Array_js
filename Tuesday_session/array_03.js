a8a92bea84b09da78d28925b37e6b1272d911298
Mohd Ziya <iamziya53@gmail.com>
2022-07-06 17:49:26 -0400

    Practice on poping and pushing

--- Tuesday_session/array_03.js
+++ Tuesday_session/array_03.js
@@ -1,4 +1,9 @@
 let num=['Mohd','Ziya','Rajesh','Prakash'];
 // console.log(num)
-console.log(num.push('Akmal'));
+// console.log(num.push('Akmal'));
+// console.log(num)
+
+console.log(num.pop());
+console.log(num);
+console.log(num.pop(2));
 console.log(num)
