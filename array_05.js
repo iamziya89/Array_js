275070bedf0137d16dda6eb95a1487e724c18ead
Mohd Ziya <iamziya53@gmail.com>
2022-07-05 20:45:11 -0400

    Practice on array

--- array_05.js
+++ array_05.js
@@ -5,11 +5,22 @@
 
 
 
+// let people=['vasanth', 'ziya','Anas'];
+
+// let people={
+//     0:'vasanth',
+//     1:'ziya',
+//     2:'Anas'
+// }
+// people[5]='Ziya'
+// console.log(people[0],people[1],people[2],people[3]);
+
+
+
+
 let people=['vasanth', 'ziya','Anas'];
 
-people={
-    0:'vasanth',
-    1:'ziya',
-    2:'Anas'
-}
+console.log(people);
+
+people[5]='Ziya';
 console.log(people);
