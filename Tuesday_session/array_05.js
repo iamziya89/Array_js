f2c2b5ba171f8cb95376cc4848b966774e11005b
Mohd Ziya <iamziya53@gmail.com>
2022-07-07 21:53:46 -0400

    practice on assignment

---
+++ Tuesday_session/array_05.js
@@ -0,0 +1,12 @@
+let num=['Mohd','Ziya','Rajesh','Prakash'];
+
+// num.unshift('ARBAZ');
+// console.log(num);
+// num.shift();
+// console.log(num);
+
+// num.push('Last');
+// console.log(num);
+
+num.pop();
+console.log(num);
