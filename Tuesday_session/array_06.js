8a901e0cbe3f48a3fcfc891be53847eb89a54008
Mohd Ziya <iamziya53@gmail.com>
2022-07-08 16:01:24 -0400

    practice on arrays

--- Tuesday_session/array_06.js
+++ Tuesday_session/array_06.js
@@ -1,4 +1,7 @@
 const num=['Mohd','Anas','Arbaz','Akmal'];
-delete num[1];
-console.log('delete',num);
-console.log(num[1]);
+// delete num[1];
+// console.log('delete',num);
+// console.log(num[1]);
+
+num[10]='Sharique'
+console.log(num)
