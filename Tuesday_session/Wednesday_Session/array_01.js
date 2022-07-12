b35e64e6b4964d91df5d78141b6b846435fbbe26
Mohd Ziya <iamziya53@gmail.com>
2022-07-12 17:18:58 -0400

    Wednesday Session

---
+++ Tuesday_session/Wednesday_Session/array_01.js
@@ -0,0 +1,7 @@
+let names=['Vasanth','Kumar','Sangakara','Dravid'];
+// console.log(names.slice(0,2));
+// console.log(names.length)
+
+
+console.log(names.slice(2,5));
+// console.log(names.length);
