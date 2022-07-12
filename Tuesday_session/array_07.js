5b3791b9a30ff9c53664116cf0328ed7b668e192
Mohd Ziya <iamziya53@gmail.com>
2022-07-12 16:15:08 -0400

    Property of splice

--- Tuesday_session/array_07.js
+++ Tuesday_session/array_07.js
@@ -1,4 +1,7 @@
 let names=['vasanth','Meraj','Shameem','Mukarrabin','Adeena']
-names.splice(2,0,'Ajhaan');
+// names.splice(2,0,'Ajhaan');
+// console.log(names);
+
+names.splice(3,1,'Ajhaan');
 console.log(names);
 
