99012426fc6411e3f4b8d1fa95796d54dce752da
Mohd Ziya <iamziya53@gmail.com>
2022-07-12 19:57:34 -0400

    Wednesday Session

--- Tuesday_session/Wednesday_Session/array_01.js
+++ Tuesday_session/Wednesday_Session/array_01.js
@@ -3,5 +3,10 @@ let names=['Vasanth','Kumar','Sangakara','Dravid'];
 // console.log(names.length)
 
 
-console.log(names.slice(2,5));
+// console.log(names.slice(2,5));
 // console.log(names.length);
+
+// console.log(names.slice(3,3));
+
+let sliceName=names.slice(1,3);
+console.log(names,sliceName);
