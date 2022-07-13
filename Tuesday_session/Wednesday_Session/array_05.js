4820266a669e5469a70efb4ece95483c88ce5cc7
Mohd Ziya <iamziya53@gmail.com>
2022-07-13 13:25:59 -0400

    Wednesday Session

---
+++ Tuesday_session/Wednesday_Session/array_05.js
@@ -0,0 +1,13 @@
+// let names=['Mohd Ziya','Kumar','Sangakara','Dravid'];
+// console.log(names.indexOf('Sangakara'));    //  2
+
+// console.log(names.indexOf('Sangakaraewd'));   // output=-1
+
+
+let names=['Mohd Ziya','Kumar','Sangakara','Dravid','Kumar'];
+// console.log(names.indexOf('Kumar'));//Gives first occurence output will be 1
+
+// console.log(names.indexOf('Kumar',2));// Seaching from index number 2
+console.log(names.indexOf('Kumar',1));
+
+
