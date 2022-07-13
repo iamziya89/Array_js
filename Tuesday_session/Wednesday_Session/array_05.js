00e8521d67f42076a209743647dd2a957c47bbde
Mohd Ziya <iamziya53@gmail.com>
2022-07-13 13:32:02 -0400

    Wednesday Session

--- Tuesday_session/Wednesday_Session/array_05.js
+++ Tuesday_session/Wednesday_Session/array_05.js
@@ -8,6 +8,9 @@ let names=['Mohd Ziya','Kumar','Sangakara','Dravid','Kumar'];
 // console.log(names.indexOf('Kumar'));//Gives first occurence output will be 1
 
 // console.log(names.indexOf('Kumar',2));// Seaching from index number 2
-console.log(names.indexOf('Kumar',1));
+// console.log(names.indexOf('Kumar',1));
+
+names=names.reverse()
+console.log(names);
 
 
