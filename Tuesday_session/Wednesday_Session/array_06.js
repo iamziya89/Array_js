3cffd1068a0a1e463c30609ae052dd84a59d8987
Mohd Ziya <iamziya53@gmail.com>
2022-07-13 15:31:29 -0400

    Wednesday Session

---
+++ Tuesday_session/Wednesday_Session/array_06.js
@@ -0,0 +1,4 @@
+let nums=[3,2,6,4,9,0];
+let names=['Amit','Aszad','Broh bhai','Mohd','Vasanth']
+console.log(nums.sort());
+console.log(names.sort());
---
+++ Tuesday_session/Wednesday_Session/array_07.js
@@ -0,0 +1,2 @@
+let nums=[3,2,6,4,9,21,12,39,1000];
+console.log(nums.sort());// Sorting only first digits number
