1f2ba179f7ed31865871cf4b34991ac0eb80eb91
Mohd Ziya <iamziya53@gmail.com>
2022-07-14 15:37:52 -0400

    Thursday Session

--- Tuesday_session/Wednesday_Session/Thursday_Session/array_03.js
+++ Tuesday_session/Wednesday_Session/Thursday_Session/array_03.js
@@ -1,3 +1,11 @@
-let nums=[1,20,32,17,89,10,27];
-nums.sort();
-console.log(nums)
+let nums=[1,20,32,17,89,10,27,100];
+// nums.sort((a,b)=>a-b);
+// console.log(nums)
+
+let max=0;
+for(let i=0;i<nums.length;i++){
+    if(max < nums[i]){
+       max=nums[i];
+    }
+}
+console.log(max);
