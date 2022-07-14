5d0d3e9239424019529b60bab7e023b85207fa50
Mohd Ziya <iamziya53@gmail.com>
2022-07-14 15:25:38 -0400

    Thursday Session

--- Tuesday_session/Wednesday_Session/Thursday_Session/array_02.js
+++ Tuesday_session/Wednesday_Session/Thursday_Session/array_02.js
@@ -1,4 +1,6 @@
 let m=19;
 let n=54;
-if(m<n)console.log(n);
-else console.log(m)
+let o=110;
+if(m>n && m>o)console.log(m);
+else if (n>m && n>o) console.log(n);
+else console.log(o);
---
+++ Tuesday_session/Wednesday_Session/Thursday_Session/array_03.js
@@ -0,0 +1,3 @@
+let nums=[1,20,32,17,89,10,27];
+nums.sort();
+console.log(nums)
