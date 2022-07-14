e6c93589c5b04de04bd68beb4fb6f666b74cd72e
Mohd Ziya <iamziya53@gmail.com>
2022-07-13 20:39:35 -0400

    Wednesday Session

---
+++ Tuesday_session/Wednesday_Session/Thursday_Session/array_01.js
@@ -0,0 +1,8 @@
+// Gratest number between two number
+
+let a=parseInt(process.argv[2]);
+let b=parseInt(process.argv[3]);
+if(a<b)
+console.log(b);
+else 
+console.log(a);
--- Tuesday_session/Wednesday_Session/array_07.js
+++ Tuesday_session/Wednesday_Session/array_07.js
@@ -1,2 +1,10 @@
-let nums=[3,2,6,4,9,21,12,39,1000];
-console.log(nums.sort());// Sorting only first digits number
+// let nums=[3,2,6,4,9,21,12,39,1000];
+// console.log(nums.sort());// Sorting only first digits number
+
+// console.log("1"+"1");
+// console.log(1+"1");
+// console.log("1"-"1");
+// console.log(1-"1");
+
+let nums=[3,2,6,4,9,21,];
+console.log(nums.sort().reverse());
