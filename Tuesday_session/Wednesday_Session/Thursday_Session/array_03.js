fb87581ccd7fba118915c54de583462c024fc3e3
Mohd Ziya <iamziya53@gmail.com>
2022-07-15 16:42:04 -0400

    Thursday Session

--- Tuesday_session/Wednesday_Session/Thursday_Session/array_03.js
+++ Tuesday_session/Wednesday_Session/Thursday_Session/array_03.js
@@ -1,8 +1,8 @@
-let nums=[1,20,32,17,89,10,27,100];
+let nums=[1,-20,-32,17,89,10,27,100];
 // nums.sort((a,b)=>a-b);
 // console.log(nums)
 
-let max=0;
+let max=nums[0];
 for(let i=0;i<nums.length;i++){
     if(max < nums[i]){
        max=nums[i];
---
+++ Tuesday_session/Wednesday_Session/Thursday_Session/array_04.js
@@ -0,0 +1,12 @@
+// let nums=[1,2,1,2,1,3,4];
+// console.log(nums.indexOf(1));
+// console.log(nums.lastIndexOf(1));
+// console.log(nums.lastIndexOf(2));
+
+// let names=['Mohd','Ziya','Anas','Arbaz']
+// // console.log(names.toString('-'));
+// console.log(names.join('-'));
+
+let names=['Mohd','Ziya','Anas','Arbaz']
+let nums=[1,2,3];
+console.log(names.concat(nums));
