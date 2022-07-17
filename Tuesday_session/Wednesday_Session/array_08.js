c1e815422e7901f9c3e45c1e889ca4e6efec064c
Mohd Ziya <iamziya53@gmail.com>
2022-07-17 10:22:15 -0400

    practice on includes methods

---
+++ Tuesday_session/Wednesday_Session/array_08.js
@@ -0,0 +1,13 @@
+let names=['sachin','kumar','sangakara','dravid'];
+
+// function findName(name){
+//     for(let i=0; i<names.length; i++){
+//         if (names[i] === name)  {return true;}
+        
+//             return false
+//     //    console.log(names[i]);
+//     }
+// }
+// console.log(findName('sangakara'));
+
+console.log(names.includes('Mohd'));
