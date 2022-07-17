d8694d16aad384a77d1198927e071862bcbbea66
Mohd Ziya <iamziya53@gmail.com>
2022-07-17 12:25:16 -0400

    Array reverse property

---
+++ Tuesday_session/Wednesday_Session/array_10.js
@@ -0,0 +1,13 @@
+// let nums=[3,4,3,2,5,1,7,1];
+// console.log(nums.indexOf(10));
+// console.log(nums.indexOf(4));
+// console.log(nums.indexOf(1));
+
+// console.log(nums.indexOf(3,1));
+
+//  Reverse of array
+// console.log(nums.reverse());
+
+let names=['vasanth','sangakara','Dravid','kumar'];
+names.reverse();
+console.log(names);
