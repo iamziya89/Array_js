43288c5f113c0d4d409a01d54319832d61fbd666
Mohd Ziya <iamziya53@gmail.com>
2022-07-19 17:09:54 -0400

    Object property

--- Tuesday_session/Wednesday_Session/Thursday_Session/Object/Obj_01.js
+++ Tuesday_session/Wednesday_Session/Thursday_Session/Object/Obj_01.js
@@ -1,6 +1,31 @@
 // OBJECT
 let circle={
     radius:1,
+    location:{
+          x:2,
+          y:2,
+    },
+    draw:function(){
 
+        return "drawing Circle";
+    },
 };
-console.log(circle["radius"]);
+// console.log(circle["radius"]);
+// // console.log(circle["location"]);
+// console.log(circle.radius);
+// console.log(circle["location"]["x"]);
+// console.log(circle.draw); //Output is [Function:draw]
+
+console.log(circle.draw(),circle.radius);
+
+let circle2={
+    radius:5,
+    location:{
+          x:7,
+          y:6,
+    },
+    draw:function(){
+     return "drawing Circle";
+    },
+};
+console.log(circle2.draw(),circle2.radius);
