89bf68971dce3a87addac38d4f7a8b560c999862
Mohd Ziya <iamziya53@gmail.com>
2022-07-22 16:03:53 -0400

    mterix operation

---
+++ Tuesday_session/Wednesday_Session/Thursday_Session/Object/12Th_july_MatrixOperation/Mat_01.js
@@ -0,0 +1,16 @@
+let matrix=[
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12],
+    [13,14,15,16]
+]
+// console.log(matrix);
+// console.log(matrix[2][2]);
+
+//  Find the number of rows and column in given matrix
+// let rows=matrix.length;
+// console.log(rows);
+
+//   Number of column preset in the matrix
+let cols=matrix[3].length;
+console.log(cols);
