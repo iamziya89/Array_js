8dcb23b2c2455ed38c5a5e4505a478ae676ca65c
Mohd Ziya <iamziya53@gmail.com>
2022-07-22 17:59:55 -0400

    mterix operation

--- Tuesday_session/Wednesday_Session/Thursday_Session/Object/12Th_july_MatrixOperation/Mat_02.js
+++ Tuesday_session/Wednesday_Session/Thursday_Session/Object/12Th_july_MatrixOperation/Mat_02.js
@@ -16,10 +16,59 @@ let matrix=[
 // }
 // console.log("The sum of diaginal element : "+sum);
 
-//  Sum of diagonal element  (Another method)
+
+// //  Sum of diagonal element  (Another method)
+// let sum=0;
+// for(let i=0; i<matrix.length; i++){
+//     sum+=matrix[i][i]
+// }
+// console.log(sum);
+
+
+// let sum=0;
+// for(let i=0; i< matrix.length; i++)
+// {
+//     for(j=matrix.length-1; j>=0;j--){
+//         if(i+j==3){
+//         sum+=matrix[i][j];
+//         }
+//     }
+// }
+// console.log(sum);
+
+// //  Sum of first column
+// let sum=0;
+// let start=0;
+// for(let i=0; i< matrix.length; i++)
+// {
+//         sum+=matrix[i][start];
+// }
+// console.log(sum);
+
+// //  Sum of second column
+// let sum=0;
+// let start=1;
+// for(let i=0; i< matrix.length; i++)
+// {
+//         sum+=matrix[i][start];
+// }
+// console.log(sum);
+
+
+// //  Sum of first rows
+// let sum=0;
+// let start=0;
+// for(let i=0; i< matrix[0].length; i++)
+// {
+//         sum+=matrix[start][i];
+// }
+// console.log(sum);
+
+//  Sum of second rows
 let sum=0;
-for(let i=0; i<matrix.length; i++){
-    sum+=matrix[i][i]
+let start=1;
+for(let i=0; i< matrix[0].length; i++)
+{
+        sum+=matrix[start][i];
 }
 console.log(sum);
-
