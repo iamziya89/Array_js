4f055ef65eac8606551a4bbec521fa2df99dad0e
Mohd Ziya <iamziya53@gmail.com>
2022-07-22 16:53:22 -0400

    mterix operation

--- Tuesday_session/Wednesday_Session/Thursday_Session/Object/12Th_july_MatrixOperation/Mat_01.js
+++ Tuesday_session/Wednesday_Session/Thursday_Session/Object/12Th_july_MatrixOperation/Mat_01.js
@@ -12,5 +12,40 @@ let matrix=[
 // console.log(rows);
 
 //   Number of column preset in the matrix
-let cols=matrix[3].length;
-console.log(cols);
+// let cols=matrix[3].length;
+// console.log(cols);
+
+//   Find the sum of all number present in the given matrix
+// let rows=matrix.length;
+// // console.log(rows);
+// let cols=matrix[0].length;
+// // console.log(cols);
+// let sum=0;
+// for(let i=0; i<rows; i++){
+//     for(let j=0; j<cols; j++){
+//         sum+=matrix[i][j];
+//     }
+// }
+// console.log(sum);
+
+
+// // Sum of first row element
+// let sum=0;
+// for(let i=0; i<matrix.length-3;i++){
+//     for(let j=0; j<matrix[0].length; j++){
+//         sum+=matrix[i][j];
+//     }
+// }
+// console.log(sum);
+
+
+
+
+//  Sum of first columns
+let sum=0;
+for(let i=0; i<matrix.length;i++){
+    for(let j=0; j<matrix[0].length-2; j++){
+        sum+=matrix[i][j];
+    }
+}
+console.log(sum);
