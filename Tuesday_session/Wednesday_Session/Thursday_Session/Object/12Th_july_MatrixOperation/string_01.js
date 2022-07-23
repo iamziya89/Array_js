53f30cc8b7709f3b3547856b66c7061bd2bfecc1
Mohd Ziya <iamziya53@gmail.com>
2022-07-23 17:55:44 -0400

    String operation

--- Tuesday_session/Wednesday_Session/Thursday_Session/Object/12Th_july_MatrixOperation/string_01.js
+++ Tuesday_session/Wednesday_Session/Thursday_Session/Object/12Th_july_MatrixOperation/string_01.js
@@ -1,8 +1,30 @@
 //    String Concept
+// let fname='Vasanth'
+// let lname='Kumar'
+// // console.log(fname+" "+lname);
+// // console.log(fname.concat(" ",lname))
+// // console.log(`${fname} ${lname}`);
+// // console.log(`${fname.length} ${lname.length}`);
+// // console.log(`"${fname.length}" ${lname}`);
+// // console.log(fname[1],lname[2]);
+// // for(let i=0; i<fname.length;i++){
+// //     console.log(fname[i]);
+// // }
+// // console.log("Characer of last name")
+// // for(let i=0;i<lname.length;i++){
+// //     console.log(lname[i])
+// // }
+
+
 let fname='Vasanth'
 let lname='Kumar'
-// console.log(fname+" "+lname);
-// console.log(fname.concat(" ",lname))
-// console.log(`${fname} ${lname}`);
-// console.log(`${fname.length} ${lname.length}`);
-console.log(`"${fname.length}" ${lname}`);
+let nums=[1,2,3]
+nums[0]=10;
+//   console.log(nums)     //Output : [10,2,3]
+
+// fname[0]='j'
+//  //  console.log(fname);    //Output : Vasanth   because string are immutable(not changable),  change only one charater it won't change
+
+// fname='AliShan'
+// console.log(fname);      AliShan
+
