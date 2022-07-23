5e67fba267df3c1c1737efd22000715c49a288ac
Mohd Ziya <iamziya53@gmail.com>
2022-07-23 17:35:26 -0400

    mterix operation

---
+++ Tuesday_session/Wednesday_Session/Thursday_Session/Object/12Th_july_MatrixOperation/string_01.js
@@ -0,0 +1,8 @@
+//    String Concept
+let fname='Vasanth'
+let lname='Kumar'
+// console.log(fname+" "+lname);
+// console.log(fname.concat(" ",lname))
+// console.log(`${fname} ${lname}`);
+// console.log(`${fname.length} ${lname.length}`);
+console.log(`"${fname.length}" '${lname}'`);
