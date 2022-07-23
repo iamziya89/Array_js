c9e8a4899c944b6f6839036c7771153373113110
Mohd Ziya <iamziya53@gmail.com>
2022-07-23 17:36:30 -0400

    String operation

--- Tuesday_session/Wednesday_Session/Thursday_Session/Object/12Th_july_MatrixOperation/string_01.js
+++ Tuesday_session/Wednesday_Session/Thursday_Session/Object/12Th_july_MatrixOperation/string_01.js
@@ -5,4 +5,4 @@ let lname='Kumar'
 // console.log(fname.concat(" ",lname))
 // console.log(`${fname} ${lname}`);
 // console.log(`${fname.length} ${lname.length}`);
-console.log(`"${fname.length}" '${lname}'`);
+console.log(`"${fname.length}" ${lname}`);
