1a1f931359a3f3cfe4995618a3d94d32a37b105b
Mohd Ziya <iamziya53@gmail.com>
2022-07-23 10:58:24 -0400

    mterix operation

---
+++ Tuesday_session/Wednesday_Session/Thursday_Session/Object/12Th_july_MatrixOperation/Mat_03.js
@@ -0,0 +1,40 @@
+let matrix=[
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12],
+    [13,14,15,16]
+]
+// let sum=0;
+// for(let i=0; i<matrix.length;i++){
+//     for(let j=0 ; j<matrix[0].length ; j++){
+//         if(i+j===3){
+//             sum+=matrix[i][j]
+//         }
+//     }
+// }
+// console.log(sum)
+
+// // Above problem from another method
+// let sum=0;
+// let start=matrix[0].length-1;
+// for(let i=0; i<matrix.length;i++){
+//     sum+=matrix[i][start];
+//     start-=1;
+// }
+// console.log(sum);
+
+
+
+// //   printing in snake method  Output like(1 2 3 4 8 7 6 5 9 10 11 12 16 15 14 13)
+// for(let i=0; i<matrix.length;i++){
+//     if(i%2==0){
+//         for(j=0;j<matrix[0].length;j++){
+//             console.log(matrix[i][j]);
+//         }
+//     }else{
+//         for(j=matrix[0].length-1;j>=0;j--){
+//             console.log(matrix[i][j]);
+//         }
+//     }
+// }
+
