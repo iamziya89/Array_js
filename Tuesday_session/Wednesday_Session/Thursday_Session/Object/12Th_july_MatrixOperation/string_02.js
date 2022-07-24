b53cd5f58e7b1f8212f6e4db0330f74f525b15be
Mohd Ziya <iamziya53@gmail.com>
2022-07-24 10:44:38 -0400

    string concept

--- Tuesday_session/Wednesday_Session/Thursday_Session/Object/12Th_july_MatrixOperation/string_02.js
+++ Tuesday_session/Wednesday_Session/Thursday_Session/Object/12Th_july_MatrixOperation/string_02.js
@@ -7,3 +7,21 @@
 // let n2=[1,2,3]
 // console.log(n1==n2);    //   in non-primitive data type its give false value
 // console.log(n1===n2);   //   in non-primitive data type its give false value
+
+// let n1=[1,2,3]
+// let n2=n1;
+// console.log(n1==n2);  //  gives true value
+
+// let fname="MOHDZIYA";
+// // let fname='Vasanthkumar'
+// console.log(fname.slice(0,7));
+// console.log(fname.slice(-12,-5));
+// console.log(fname.charAt(0));
+// console.log(fname[0],fname.charAt(0));
+
+
+let fname="MOHDZIYA";
+// console.log(fname.slice(4));
+console.log(fname.substring(-5,-2))
+
+
